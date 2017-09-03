3f6d73d81b995ccbf8749936eaea921d1a2c4e23
matthewb2 <bestsciencekwabe@gmail.com>
2017-09-04 02:09:41 +0900

    CSDownload

---
+++ CSDownload/CSDownload/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace CSDownload
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.prgDownload = new System.Windows.Forms.ProgressBar();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblDownload = new System.Windows.Forms.Label();
+            this.btnLocation = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // prgDownload
+            // 
+            this.prgDownload.Location = new System.Drawing.Point(-2, 190);
+            this.prgDownload.Name = "prgDownload";
+            this.prgDownload.Size = new System.Drawing.Size(498, 23);
+            this.prgDownload.TabIndex = 0;
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(98, 27);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(384, 21);
+            this.txtUrl.TabIndex = 1;
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(98, 63);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(384, 21);
+            this.txtPath.TabIndex = 2;
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(282, 110);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(75, 23);
+            this.btnDownload.TabIndex = 4;
+            this.btnDownload.Text = "다운로드";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click_1);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(363, 110);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "중지";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "다운로드 URL";
+            // 
+            // lblDownload
+            // 
+            this.lblDownload.AutoSize = true;
+            this.lblDownload.Location = new System.Drawing.Point(5, 175);
+            this.lblDownload.Name = "lblDownload";
+            this.lblDownload.Size = new System.Drawing.Size(29, 12);
+            this.lblDownload.TabIndex = 8;
+            this.lblDownload.Text = "준비";
+            // 
+            // btnLocation
+            // 
+            this.btnLocation.Location = new System.Drawing.Point(12, 63);
+            this.btnLocation.Name = "btnLocation";
+            this.btnLocation.Size = new System.Drawing.Size(75, 23);
+            this.btnLocation.TabIndex = 9;
+            this.btnLocation.Text = "저장폴더";
+            this.btnLocation.UseVisualStyleBackColor = true;
+            this.btnLocation.Click += new System.EventHandler(this.btnLocation_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 262);
+            this.Controls.Add(this.btnLocation);
+            this.Controls.Add(this.lblDownload);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.prgDownload);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar prgDownload;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblDownload;
+        private System.Windows.Forms.Button btnLocation;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ CSDownload/CSDownload/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Threading;
+
+
+
+namespace CSDownload
+{
+    public partial class Form1 : Form
+    {
+        // The thread inside which the download happens
+        private Thread thrDownload;
+        // The stream of data retrieved from the web server
+        private Stream strResponse;
+        // The stream of data that we write to the harddrive
+        private Stream strLocal;
+        // The request to the web server for file information
+        private HttpWebRequest webRequest;
+        // The response from the web server containing information about the file
+        private HttpWebResponse webResponse;
+        // The progress of the download in percentage
+        private static int PercentProgress;
+        // The delegate which we will call from the thread to update the form
+        private delegate void UpdateProgessCallback(Int64 BytesRead, Int64 TotalBytes);
+        string path;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void UpdateProgress(Int64 BytesRead, Int64 TotalBytes)
+        {
+            // Calculate the download progress in percentages
+            PercentProgress = Convert.ToInt32((BytesRead * 100) / TotalBytes);
+            // Make progress on the progress bar
+            prgDownload.Value = PercentProgress;
+            // Display the current progress on the form
+            lblDownload.Text = "Downloaded " + BytesRead + " out of " + TotalBytes + " (" + PercentProgress + "%)";
+        }
+
+        private void Download()
+        {
+            using (WebClient wcDownload = new WebClient())
+            {
+                try
+                {
+                    // Create a request to the file we are downloading
+                    webRequest = (HttpWebRequest)WebRequest.Create(txtUrl.Text);
+                    // Set default authentication for retrieving the file
+                    webRequest.Credentials = CredentialCache.DefaultCredentials;
+                    // Retrieve the response from the server
+                    webResponse = (HttpWebResponse)webRequest.GetResponse();
+                    // Ask the server for the file size and store it
+                    Int64 fileSize = webResponse.ContentLength;
+
+                    // txtPath.Text = "C:\\temp\\";
+                    Uri uri = new Uri(txtUrl.Text);
+                    string filename = System.IO.Path.GetFileName(uri.LocalPath);
+                    // Open the URL for download
+                    strResponse = wcDownload.OpenRead(txtUrl.Text);
+                    // Create a new file stream where we will be saving the data (local drive)
+                    // strLocal = new FileStream("C:\\temp\\"+ filename, FileMode.Create, FileAccess.Write, FileShare.None);
+                    strLocal = new FileStream(path + filename, FileMode.Create, FileAccess.Write, FileShare.None);
+
+                    // It will store the current number of bytes we retrieved from the server
+                    int bytesSize = 0;
+                    // A buffer for storing and writing the data retrieved from the server
+                    byte[] downBuffer = new byte[2048];
+
+                    // Loop through the buffer until the buffer is empty
+                    while ((bytesSize = strResponse.Read(downBuffer, 0, downBuffer.Length)) > 0)
+                    {
+                        // Write the data from the buffer to the local hard drive
+                        strLocal.Write(downBuffer, 0, bytesSize);
+                        // Invoke the method that updates the form's label and progress bar
+                        this.Invoke(new UpdateProgessCallback(this.UpdateProgress), new object[] { strLocal.Length, fileSize });
+                    }
+                }
+                finally
+                {
+                    // When the above code has ended, close the streams
+                    strResponse.Close();
+                    strLocal.Close();
+                }
+            }
+        }
+
+      
+
+        private void btnDownload_Click_1(object sender, EventArgs e)
+        {
+            // Let the user know we are connecting to the server
+            lblDownload.Text = "Download Starting";
+            // Create a new thread that calls the Download() method
+            thrDownload = new Thread(Download);
+            // Start the thread, and thus call Download()
+            thrDownload.Start();
+        }
+
+        private void btnStop_Click_1(object sender, EventArgs e)
+        {
+            // Close the web response and the streams
+            webResponse.Close();
+            strResponse.Close();
+            strLocal.Close();
+            // Abort the thread that's downloading
+            thrDownload.Abort();
+            // Set the progress bar back to 0 and the label
+            prgDownload.Value = 0;
+            lblDownload.Text = "Download Stopped";
+        }
+
+        private void btnLocation_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+
+            folderDlg.ShowNewFolderButton = true;
+
+            // Show the FolderBrowserDialog.
+
+            DialogResult result = folderDlg.ShowDialog();
+
+            if (result == DialogResult.OK)
+
+            {
+
+                path = folderDlg.SelectedPath;
+
+                txtPath.Text = path;
+
+                //  Environment.SpecialFolder root = folderDlg.RootFolder;
+
+            }
+        }
+    }
+
+
+
+}
