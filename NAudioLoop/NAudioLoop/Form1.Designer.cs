50ba8e32fea70639dc1f9a7fed920f78a1af5afc
matthewb2 <bestsciencekwabe@gmail.com>
2019-03-06 22:40:12 +0900

    merge with video and audio

---
+++ NAudioLoop/NAudioLoop/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace NAudioLoop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStartStop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonStartStop
+            // 
+            this.buttonStartStop.Location = new System.Drawing.Point(129, 160);
+            this.buttonStartStop.Name = "buttonStartStop";
+            this.buttonStartStop.Size = new System.Drawing.Size(151, 46);
+            this.buttonStartStop.TabIndex = 0;
+            this.buttonStartStop.Text = "변환";
+            this.buttonStartStop.UseVisualStyleBackColor = true;
+            this.buttonStartStop.Click += new System.EventHandler(this.buttonStartStop_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "영상";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "음성";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(337, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "열기";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(337, 60);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "열기";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(70, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(261, 21);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(70, 60);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(261, 21);
+            this.textBox2.TabIndex = 6;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(337, 123);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "저장";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(70, 123);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(261, 21);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.Text = "D:\\out.mp4";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "파일";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 218);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonStartStop);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStartStop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ NAudioLoop/NAudioLoop/Form1.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using NAudio.Wave;
+
+using Microsoft.WindowsAPICodePack.Shell.PropertySystem;
+using Microsoft.WindowsAPICodePack.Shell;
+using System.Threading;
+
+
+using System.IO;
+
+namespace NAudioLoop
+{
+
+    public partial class Form1 : Form
+    {
+
+        static EventWaitHandle _waitHandle = new AutoResetEvent(false);
+
+
+        private WaveOut waveOut;
+
+        string strFileName;
+        string strFilePath;
+
+      
+
+        public static void Mp3ToWav(string mp3File, string outputFile)
+        {
+            try
+            {
+                using (Mp3FileReader reader = new Mp3FileReader(mp3File))
+                {
+                    using (WaveStream pcmStream = WaveFormatConversionStream.CreatePcmStream(reader))
+                    {
+                        WaveFileWriter.CreateWaveFile(outputFile, pcmStream);
+                    }
+                }
+            }
+            catch {
+                MessageBox.Show("can not make a output");
+            }
+        }
+
+        public TimeSpan GetVideoDuration(string filePath)
+        {
+            ulong t;
+            try
+            {
+                using (var shell = ShellObject.FromParsingName(filePath))
+                {
+                    IShellProperty prop = shell.Properties.System.Media.Duration;
+                    t = (ulong)prop.ValueAsObject;
+                    return TimeSpan.FromTicks((long)t);
+                }
+            }
+            catch { }
+            return new TimeSpan(0);
+
+        }
+
+
+        public static void Concatenate(string outputFile, ArrayList sourceFiles)
+        {
+            byte[] buffer = new byte[1024];
+            WaveFileWriter waveFileWriter = null;
+
+            try
+            {
+                foreach (string sourceFile in sourceFiles)
+                {
+                    using (WaveFileReader reader = new WaveFileReader(sourceFile))
+                    {
+                        if (waveFileWriter == null)
+                        {
+                            // first time in create new Writer
+                            waveFileWriter = new WaveFileWriter(outputFile, reader.WaveFormat);
+                        }
+                        else
+                        {
+                            if (!reader.WaveFormat.Equals(waveFileWriter.WaveFormat))
+                            {
+                                throw new InvalidOperationException("Can't concatenate WAV Files that don't share the same format");
+                            }
+                        }
+
+                        int read;
+                        while ((read = reader.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            waveFileWriter.WriteData(buffer, 0, read);
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                if (waveFileWriter != null)
+                {
+                    waveFileWriter.Dispose();
+                }
+            }
+
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+
+
+        public void MyMethod(ArrayList sf)
+        {
+            //... (execute any other action)
+
+            Concatenate(@"C:\Output.wav", sf);
+            _waitHandle.Set();
+        }
+
+        public void MyMethod2()
+        {
+            //... (execute any other action)
+
+            //Concatenate(@"C:\Output.wav", sf);
+            Mp3ToWav(textBox2.Text, @"C:\test.wav");
+            _waitHandle.Set();
+        }
+
+        
+
+
+        private void buttonStartStop_Click_1(object sender, EventArgs e)
+        {
+
+            ThreadStart tStart = new ThreadStart(() => MyMethod2());
+
+            Thread t = new Thread(tStart);
+            t.Start();
+
+            t.Join();
+            _waitHandle.WaitOne();
+
+
+            //Mp3ToWav(textBox2.Text, @"C:\test.wav");
+            //MessageBox.Show("audio file is ready");
+
+            string music_src = @"C:\test.wav";
+            WaveFileReader reader = new WaveFileReader(music_src);
+
+            TimeSpan ts = reader.TotalTime;
+
+
+            // label1.Text = String.Format("총 재생시간 {0}:{0}", ts.Minutes, ts.Seconds);
+
+            ArrayList sf = new ArrayList();
+
+
+            TimeSpan ts2 = GetVideoDuration(textBox1.Text);
+
+            //MessageBox.Show(ts2.TotalSeconds.ToString());
+
+            // label1.Text = String.Format("총 재생시간 {0}:{0}", ts2.Minutes, ts2.Seconds);
+            //TimeSpan ts2 = new TimeSpan(0);
+                       
+            TimeSpan tout = new TimeSpan(0);
+
+            while (ts2.TotalSeconds > tout.TotalSeconds)
+            {
+                sf.Add(music_src);
+                //sf.Add(@"C:\Example.wav");
+                tout += ts;
+                // MessageBox.Show(tout.TotalSeconds.ToString());
+            }
+
+
+            tStart = new ThreadStart(() => MyMethod(sf));
+
+            t = new Thread(tStart);
+            t.Start();
+
+            t.Join();
+            _waitHandle.WaitOne();
+            if (tout.TotalSeconds > ts2.TotalSeconds)
+            {
+                string endpoint = ts2.Hours + ":" + ts2.Minutes + ":" + ts2.Seconds;
+
+                string files = "-y -ss 00:00:00 -i C:\\Output.wav -t " + endpoint + " C:\\Output2.wav";
+                //MessageBox.Show(files);
+                string cmdtext = "/C ffmpeg " + files;
+                var process = System.Diagnostics.Process.Start("CMD.exe", cmdtext);
+                process.WaitForExit();
+                string cmdtext2 = "/C ffmpeg -y -i C:\\Output2.wav -filter:a volume=0.3 C:\\Output3.wav";
+                var process2 = System.Diagnostics.Process.Start("CMD.exe", cmdtext2);
+                process2.WaitForExit();
+
+            }
+            else
+            {
+                // ffmpeg -i input.wav -filter:a "volume=0.5" output.wav
+                string cmdtext2 = "/C ffmpeg -y -i C:\\test.wav -filter:a volume=0.2 C:\\Output3.wav";
+                var process2 = System.Diagnostics.Process.Start("CMD.exe", cmdtext2);
+                process2.WaitForExit();
+            }
+
+          
+
+            // string cmdtext3 = "/C ffmpeg -y -i "+ fullPath + " -i C:\\Output3.wav -filter_complex [0:a][1:a]amix=duration=shortest[a] -map 0:v -map [a] -c:v copy C:\\newavoutput.mp4";
+            string cmdtext3 = "/C ffmpeg -y -i " + textBox1.Text.Replace(@"\", @"\\") + " -i C:\\Output3.wav -filter_complex [0:a][1:a]amix=duration=shortest[a] -map 0:v -map [a] -c:v copy " + textBox3.Text.Replace(@"\", @"\\");
+
+            var process3 = System.Diagnostics.Process.Start("CMD.exe", cmdtext3);
+            process3.WaitForExit();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+
+                System.IO.FileInfo fInfo = new System.IO.FileInfo(openFileDialog1.FileName);
+
+                strFileName = fInfo.Name;
+
+                strFilePath = fInfo.DirectoryName;
+
+                //MessageBox.Show(strFileName + ", " + strFilePath);
+
+                textBox1.Text = strFilePath + strFileName;
+                // textBox2.Text = fInfo.DirectoryName + "\\" + fInfo.Name;
+
+
+                // sr.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog2.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+
+                System.IO.FileInfo fInfo = new System.IO.FileInfo(openFileDialog2.FileName);
+
+                string astrFileName = fInfo.Name;
+
+                string astrFilePath = fInfo.DirectoryName;
+
+                textBox2.Text = astrFilePath + "\\"+astrFileName;
+
+                //textBox2.Text = astrFilePath + "\\" + astrFileName;
+                textBox2.Text = textBox2.Text.Replace(@"\\", @"\");
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Displays a SaveFileDialog so the user can save the Image  
+            // assigned to Button2.  
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Mpeg4 Video|*.mp4";
+            saveFileDialog1.Title = "Save an Mpeg4 File";
+            saveFileDialog1.ShowDialog();
+
+            // If the file name is not an empty string open it for saving.  
+            if (saveFileDialog1.FileName != "")
+            {
+                // Saves the Image via a FileStream created by the OpenFile method.  
+                System.IO.FileStream fs =
+                   (System.IO.FileStream)saveFileDialog1.OpenFile();
+                // Saves the Image in the appropriate ImageFormat based upon the  
+                // File type selected in the dialog box.  
+                // NOTE that the FilterIndex property is one-based.  
+                // Get file name.
+                string name = saveFileDialog1.FileName;
+                textBox3.Text = name;
+
+                fs.Close();
+            }
+        }
+    }
+}
+
